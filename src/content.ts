363e5a3c3e1f9f438c07c3d3b4b3ea9a5bcb313a
nmhillusion <nguyenminhhieu.geek@gmail.com>
2022-09-30 19:47:05 +0700

    fixed autoplay video with larger video mode

--- src/content.ts
+++ src/content.ts
@@ -1,5 +1,9 @@
 const STORE = {
   playedVideos: [] as string[],
+  CURRENT_STATE: {
+    videoEl: null as HTMLVideoElement,
+    parentVideoNode: null as HTMLElement,
+  },
 };
 const URL_PATTERN = /https?:\/\/\w+\.facebook\.com\/watch.+?/;
 
@@ -19,25 +23,61 @@ function cssContent() {
   `;
 }
 
+function onEndedVideo(videos: HTMLVideoElement[], v: HTMLVideoElement) {
+  console.log("video ended: ", v);
+  v.remove();
+  document.querySelector(".n2-video-container")?.remove();
+
+  if (location.href.match(URL_PATTERN)) {
+    startNewVideo(videos);
+  } else {
+    console.warn("url is not match to video pattern: ", location.href);
+  }
+}
+
+function onExitLargerVideoMode(
+  oldParentVideo: HTMLElement,
+  v: HTMLVideoElement
+) {
+  if (!oldParentVideo) {
+    oldParentVideo = STORE.CURRENT_STATE.parentVideoNode;
+  }
+
+  if (!v) {
+    v = STORE.CURRENT_STATE.videoEl;
+  }
+
+  console.log("on click exit full size of video: ", v);
+
+  if (v && oldParentVideo) {
+    v.style.position = "static";
+    v.style.height = "inherit";
+
+    oldParentVideo?.appendChild(v);
+    // console.log("v.parentElement: ", v.parentElement);
+  }
+  document.querySelector(".n2-video-container")?.remove();
+}
+
 function main() {
   const videos = nodeListToArray(document.querySelectorAll("video"));
   console.log({ videos });
 
+  if (STORE.CURRENT_STATE.videoEl) {
+    if (isAlmostEndVideo(STORE.CURRENT_STATE.videoEl)) {
+      onExitLargerVideoMode(null, null);
+    }
+  }
+
   videos.forEach((v) => {
     v.onended = (e) => {
-      console.log("video ended: ", v);
-      v.remove();
-      document.querySelector(".n2-video-container")?.remove();
-
-      if (location.href.match(URL_PATTERN)) {
-        startNewVideo(videos);
-      } else {
-        console.warn("url is not match to video pattern: ", location.href);
-      }
+      onEndedVideo(videos, v);
     };
 
     v.onplaying = async (e) => {
       console.log("on playing video: ", v);
+      STORE.CURRENT_STATE.videoEl = v;
+      STORE.CURRENT_STATE.parentVideoNode = v.parentElement;
 
       if (!STORE.playedVideos.find((sv) => sv === v.src)) {
         STORE.playedVideos.push(v.src);
@@ -59,6 +99,11 @@ function startNewVideo(videos: HTMLVideoElement[]) {
   for (const v of videos) {
     if (!STORE.playedVideos.includes(v.src)) {
       console.log("prepare to start for video: ", v);
+      v.scrollIntoView({
+        inline: "start",
+        behavior: "smooth",
+        block: "start",
+      });
       v.focus();
 
       const looper = setTimeout(() => {
@@ -85,7 +130,7 @@ async function startLargerVideoMode(
       vL.style.height = "inherit";
     });
 
-    v.parentElement?.remove();
+    const oldParentVideo = v.parentElement;
 
     const playerContainer = document.createElement("div");
     playerContainer.classList.add("n2-video-container");
@@ -98,13 +143,7 @@ async function startLargerVideoMode(
       playerContainer.appendChild(btnExit);
 
       btnExit.onclick = (e) => {
-        console.log("on click exit full size of video: ", v);
-
-        v.style.position = "static";
-        v.style.height = "inherit";
-
-        document.querySelector(".n2-video-container")?.remove();
-        v.parentElement?.appendChild(v);
+        onExitLargerVideoMode(oldParentVideo, v);
       };
     }
     document.body.appendChild(playerContainer);
@@ -112,13 +151,23 @@ async function startLargerVideoMode(
 }
 
 function ableToStartLargerVideoMode(v: HTMLVideoElement) {
+  console.log("remain time: ", v.duration - v.currentTime);
+
   return 10 <= v.duration - v.currentTime;
 }
 
-() => {
+function isAlmostEndVideo(v: HTMLVideoElement) {
+  if (v) {
+    return 10 > v.duration - v.currentTime;
+  } else {
+    return true;
+  }
+}
+
+(() => {
   STORE.playedVideos = [];
 
   setInterval(() => {
     main();
   }, 3000);
-};
+})();
